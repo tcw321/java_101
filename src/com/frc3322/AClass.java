10be9f09e6a349160bf1a26ec9d848909f30bd2b
Timothy C. Wright <tcw321@gmail.com>
2018-10-06 13:45:25 -0400

    constructor passed

--- src/com/frc3322/AClass.java
+++ src/com/frc3322/AClass.java
@@ -4,6 +4,12 @@ public class AClass {
     public int val = 1;
     private int privateVal = 2;
 
+    public AClass() {}
+
+    public AClass(int i) {
+        privateVal = i;
+    }
+
     public int getPrivateVal() {
         return privateVal;
     }
