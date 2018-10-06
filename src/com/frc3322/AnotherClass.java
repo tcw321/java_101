4b0e8ce8b53b37defcba64d4719af8dfd0e0e745
Timothy C. Wright <tcw321@gmail.com>
2018-10-06 14:00:51 -0400

    referencing another object passed

---
+++ src/com/frc3322/AnotherClass.java
@@ -0,0 +1,21 @@
+package com.frc3322;
+
+public class AnotherClass {
+    private int val;
+
+    public AnotherClass(int val)
+    {
+        this.val = val;
+    }
+
+    public void setVal(int val)
+    {
+        this.val = val;
+    }
+
+    public int getVal()
+    {
+        return this.val;
+    }
+
+}
