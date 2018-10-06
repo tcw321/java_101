25aa65d8bf2ee917dac9544f2f4385ad76811b9a
Timothy C. Wright <tcw321@gmail.com>
2018-10-06 14:07:11 -0400

    Robot class that implements a interface

---
+++ tests/com/frc3322/RobotTests.java
@@ -0,0 +1,13 @@
+package com.frc3322;
+
+import org.junit.Test;
+
+public class RobotTests {
+
+    @Test
+    public void interfaceRobot()
+    {
+        //InterfaceRobot robot = new InterfaceRobot();
+        RobotFromInterface robot = new RobotFromInterface();
+    }
+}
